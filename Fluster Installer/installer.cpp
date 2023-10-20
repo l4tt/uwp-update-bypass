e2a89e9be19fdab016b7c04abb853791849d07b3
Pixeluted <pixeluted@skiff.com>
2023-10-20 14:12:39 +0200

    Added progress bar for dowloading Extracting now takes the real time instead of predefined delay Installer is now able to detect corrupted installation or detect if fluster is already installed. Application now doesn't close right away to give the user chance to see what happened Fixed grammatical errors

--- Fluster Installer/installer.cpp
+++ Fluster Installer/installer.cpp
@@ -7,15 +7,32 @@
 #include "console.h"
 #include <Shlobj.h>
 #include <Shlwapi.h>
+#include <iomanip>
 
+void ShowProgressBar(double percent, double downloadedMB, double totalMB) {
+    const int barWidth = 50;
+    std::cout << "[";
 
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < static_cast<int>(barWidth * percent / 100.0)) {
+            std::cout << "=";
+        }
+        else {
+            std::cout << " ";
+        }
+    }
+
+    std::cout << "] " << std::fixed << std::setprecision(2) << percent << "% ";
+    std::cout << downloadedMB << "MB/" << totalMB << "MB\r";
+    std::cout.flush();
+}
 
 bool DownloadFileToCDrive(const std::string& url, const std::string& destinationDir) {
     HINTERNET hInternet, hConnect;
     DWORD bytesRead;
     std::string fileName = url.substr(url.find_last_of("/") + 1);
     std::string destinationPath = destinationDir + "\\" + fileName;
-    
+
     hInternet = InternetOpen(L"DownloadFileToCDrive", INTERNET_OPEN_TYPE_DIRECT, nullptr, nullptr, 0);
     if (hInternet == nullptr) {
         ErrorMessage("Failed to Download File ");
@@ -28,39 +45,54 @@ bool DownloadFileToCDrive(const std::string& url, const std::string& destination
         InternetCloseHandle(hInternet);
         return false;
     }
+
+    DWORD fileSize = 0;
+    DWORD dwSize = sizeof(fileSize);
+    if (!HttpQueryInfo(hConnect, HTTP_QUERY_CONTENT_LENGTH | HTTP_QUERY_FLAG_NUMBER, &fileSize, &dwSize, NULL)) {
+        ErrorMessage("Failed to get file size.");
+    }
+
+    DWORD totalBytesRead = 0;
+    double totalMB = fileSize / (1024.0 * 1024.0);
+
     if (!CreateDirectoryA(destinationDir.c_str(), nullptr) && GetLastError() != ERROR_ALREADY_EXISTS) {
-        ErrorMessage("Looks like you already have fluster installed please delete the path: ");
+        ErrorMessage("Looks like you already have fluster installed, please delete the path: ");
         return false;
     }
 
     std::ofstream outputFile(destinationPath, std::ios::binary);
     if (!outputFile) {
-        std::cerr << "Failed to create or open the destination file." << std::endl;
+        ErrorMessage("Failed to create or open the destination file.");
         InternetCloseHandle(hConnect);
         InternetCloseHandle(hInternet);
         return false;
     }
 
-    SuccessMessage("Created Fluter install path: " + destinationDir);
-   
-    SuccessMessage("Starting to Download Fluter Binary... ");
+    SuccessMessage("Created Fluster installation path: " + destinationDir);
+    SuccessMessage("Starting to Download Fluster Binary... ");
 
     char buffer[4096];
     while (InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0) {
         outputFile.write(buffer, bytesRead);
+
+        totalBytesRead += bytesRead;
+        double percent = (static_cast<double>(totalBytesRead) / fileSize) * 100.0;
+        double downloadedMB = totalBytesRead / (1024.0 * 1024.0);
+        ShowProgressBar(percent, downloadedMB, totalMB);
     }
 
+    std::cout << std::endl;
     outputFile.close();
     InternetCloseHandle(hConnect);
     InternetCloseHandle(hInternet);
 
-    SuccessMessage("Downloaded the Fluter Binary in " + destinationPath);
-
+    SuccessMessage("Downloaded the Fluster Binary in " + destinationPath);
 
     RenameAndExtractToFolder(destinationPath, destinationDir);
-    
+
     return true;
 }
+
 
 
 bool AddAppxPackage(const std::string& appxManifestPath) {
@@ -79,32 +111,47 @@ bool AddAppxPackage(const std::string& appxManifestPath) {
     }
 }
 
-
-
 bool ExtractZipFile(const std::string& zipFilePath, const std::string& extractPath) {
-    
-    std::wstring expandCommand = L"Expand-Archive \"" + std::wstring(zipFilePath.begin(), zipFilePath.end()) + L"\" -DestinationPath \"" + std::wstring(extractPath.begin(), extractPath.end()) + L"\"";
+    std::wstring expandCommand = L"Expand-Archive \"" + std::wstring(zipFilePath.begin(), zipFilePath.end()) +
+        L"\" -DestinationPath \"" + std::wstring(extractPath.begin(), extractPath.end()) + L"\"";
 
+    std::wstring commandLine = L"powershell.exe " + expandCommand;
 
-    HINSTANCE result = ShellExecuteW(nullptr, L"open", L"powershell.exe", expandCommand.c_str(), nullptr, SW_HIDE);
+    wchar_t* commandLineNonConst = new wchar_t[commandLine.length() + 1];
+    wcscpy_s(commandLineNonConst, commandLine.length() + 1, commandLine.c_str());
 
-    if ((int)result > 32) {
-        SuccessMessage("Extracting Fluster Binary Please wait 90s");
-        WarningMessage("This May take awhile depending on your SSD/Hardrive");
+    STARTUPINFOW si = {};
+    si.cb = sizeof(si);
+    PROCESS_INFORMATION pi = {};
 
-        Sleep(90000);
-        AddAppxPackage(extractPath + "\\AppxManifest.xml");
-        return true;
+    if (CreateProcessW(NULL, commandLineNonConst, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        SuccessMessage("Extracting Fluster Binary. Please wait...");
+        WarningMessage("This may take a while depending on your SSD/Hard drive.");
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+
+        if (DeleteFileW(std::wstring(zipFilePath.begin(), zipFilePath.end()).c_str())) {
+            delete[] commandLineNonConst;
+            
+            AddAppxPackage(extractPath + "\\AppxManifest.xml");
+        }
+        else {
+            delete[] commandLineNonConst;
+
+            return false;
+        }
     }
     else {
-        ErrorMessage("Failed to Extract Flusters Binary");
+        ErrorMessage("Failed to extract Fluster's binary");
+
+        delete[] commandLineNonConst;
+
         return false;
     }
 }
-
-
-
-
 
 bool RenameAndExtractToFolder(const std::string& sourceFilePath, const std::string& destinationFolder) {
 
--- Fluster Installer/main.cpp
+++ Fluster Installer/main.cpp
@@ -17,29 +17,99 @@ bool EnableDeveloperMode()
     
 }
 
+std::pair<bool, bool> IsFlusterInstalled(const std::string& dirPath, const std::string& fileName) {
+    bool dirExists = false;
+    bool fileExists = false;
+
+    DWORD attribs = GetFileAttributesA(dirPath.c_str());
+    if (attribs != INVALID_FILE_ATTRIBUTES) {
+        dirExists = (attribs & FILE_ATTRIBUTE_DIRECTORY);
+    }
+
+    std::string fullPath = dirPath + "\\" + fileName;
+    attribs = GetFileAttributesA(fullPath.c_str());
+    if (attribs != INVALID_FILE_ATTRIBUTES) {
+        fileExists = true;
+    }
+
+    return { dirExists, fileExists };
+}
+
+bool DeleteDirectoryRecursively(const std::string& path) {
+    WIN32_FIND_DATAA findFileData;
+    HANDLE hFind = FindFirstFileA((path + "\\*").c_str(), &findFileData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        return false;
+    }
+    do {
+        const std::string fileOrDir = path + "\\" + findFileData.cFileName;
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            if (strcmp(findFileData.cFileName, ".") != 0 &&
+                strcmp(findFileData.cFileName, "..") != 0) {
+                DeleteDirectoryRecursively(fileOrDir);
+            }
+        }
+        else {
+            DeleteFileA(fileOrDir.c_str());
+        }
+    } while (FindNextFileA(hFind, &findFileData) != 0);
+    FindClose(hFind);
+
+    return RemoveDirectoryA(path.c_str());
+}
+
 int main()
 {
     IntroMessage();
     WarningMessage("Installer made by Nano - discord.gg/runesoftware");
     WarningMessage("Big thanks to cereal for making uwp update bypasser!\n\n");
 
+    std::string url = "https://github.com/cerealwithmilk/uwp/releases/download/test/Fluster.msix";
+    std::string fileName = "Windows10Universal.exe";
+    std::string destinationDir = "C:\\Fluster";
+
+    std::pair<bool, bool> flusterCheck = IsFlusterInstalled(destinationDir, fileName);
+
+    if (flusterCheck.first) {
+        // Fluster directory exists
+        if (flusterCheck.second) {
+            // Fluster is installed!
+            WarningMessage("Fluster is already installed! If you want to reinstall, delete the Fluster folder in C drive ");
+            system("pause");
+            return 0;
+        }
+        else {
+            // Corruputed installation
+            if (DeleteDirectoryRecursively(destinationDir.c_str())) {
+                WarningMessage("Fluster is installed but corrupted, Fluster will be re-installed ");
+            }
+            else {
+                WarningMessage("Failed to delete the Fluster directory to start re-installation ");
+                system("pause");
+                return 0;
+            }
+        }
+    }
+
     if (!EnableDeveloperMode())
     {
         ErrorMessage("Failed to enable Developer Mode.");
-        
+        system("pause");
+        return 0;
+
     }
 
-    SuccessMessage("Enabled Microsoft Developer Setting Enabled");
-
-
-    std::string url = "https://github.com/cerealwithmilk/uwp/releases/download/test/Fluster.msix";
-    std::string destinationDir = "C:\\Fluster";
+    SuccessMessage("Sucessfully enabled developer mode");
 
     if (DownloadFileToCDrive(url, destinationDir)) {
         SuccessMessage("Thank you for using Fluster - Rune on top / Byfron == Bad");
     }
     else {
-        ErrorMessage("Failed........ to Download Fluster");
+        ErrorMessage("Failed to Download Fluster");
     }
+
+    system("pause");
+
     return 0;
 }
