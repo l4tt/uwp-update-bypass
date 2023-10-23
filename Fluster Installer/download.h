5abd6c3931947063b2c02dcee40103728b519c9e
Pixeluted <pixeluted@skiff.com>
2023-10-24 01:53:32 +0200

    Added notification to the installer

--- Fluster Installer/download.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-bool DownloadFileToCDrive(const std::string& url, const std::string& destinationDir);
-bool RenameAndExtractToFolder(const std::string& sourceFilePath, const std::string& destinationFolder);
